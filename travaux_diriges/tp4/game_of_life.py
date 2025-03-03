78f9e63d4756326ffeeaacb8e1440d0f3d80cf5b
Guilherme Gelmi <ggelmisalvo@gmail.com>
2025-03-03 16:07:13 +0100

    game of live 1

--- travaux_diriges/tp4/game_of_life.py
+++ travaux_diriges/tp4/game_of_life.py
@@ -23,6 +23,7 @@ On itère ensuite pour étudier la façon dont évolue la population des cellule
 """
 import pygame as pg
 import numpy as np
+from mpi4py import MPI
 
 
 class Grille:
@@ -70,7 +71,6 @@ class App:
     """
 
     def __init__(self, geometry, grid):
-        self.grid = grid
         # Calcul de la taille d'une cellule par rapport à la taille de la fenêtre et de la grille à afficher :
         self.size_x = geometry[1] // grid.dimensions[1]
         self.size_y = geometry[0] // grid.dimensions[0]
@@ -83,18 +83,17 @@ class App:
         self.height = grid.dimensions[0] * self.size_y
         # Création de la fenêtre à l'aide de tkinter
         self.screen = pg.display.set_mode((self.width, self.height))
-        #
         self.canvas_cells = []
-        self.colors = np.array([self.grid.col_dead[:-1], self.grid.col_life[:-1]])
+        self.colors = np.array([grid.col_dead[:-1], grid.col_life[:-1]])
 
-    def draw(self):
-        surface = pg.surfarray.make_surface(self.colors[self.grid.cells.T])
+    def draw(self, grid):
+        surface = pg.surfarray.make_surface(self.colors[grid.cells.T])
         surface = pg.transform.flip(surface, False, True)
         surface = pg.transform.scale(surface, (self.width, self.height))
         self.screen.blit(surface, (0, 0))
         if (self.draw_color is not None):
-            [pg.draw.line(self.screen, self.draw_color, (0, i * self.size_y), (self.width, i * self.size_y)) for i in range(self.grid.dimensions[0])]
-            [pg.draw.line(self.screen, self.draw_color, (j * self.size_x, 0), (j * self.size_x, self.height)) for j in range(self.grid.dimensions[1])]
+            [pg.draw.line(self.screen, self.draw_color, (0, i * self.size_y), (self.width, i * self.size_y)) for i in range(grid.dimensions[0])]
+            [pg.draw.line(self.screen, self.draw_color, (j * self.size_x, 0), (j * self.size_x, self.height)) for j in range(grid.dimensions[1])]
         pg.display.update()
 
 
@@ -102,52 +101,76 @@ if __name__ == '__main__':
     import time
     import sys
 
-    pg.init()
-    dico_patterns = {  # Dimension et pattern dans un tuple
-        "blinker": ((5, 5), [(2, 1), (2, 2), (2, 3)]),
-        "toad": ((6, 6), [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (3, 5)]),
-        "acorn": ((100, 100), [(51, 52), (52, 54), (53, 51), (53, 52), (53, 55), (53, 56), (53, 57)]),
-        "beacon": ((6, 6), [(1, 3), (1, 4), (2, 3), (2, 4), (3, 1), (3, 2), (4, 1), (4, 2)]),
-        "boat": ((5, 5), [(1, 1), (1, 2), (2, 1), (2, 3), (3, 2)]),
-        "glider": ((100, 90), [(1, 1), (2, 2), (2, 3), (3, 1), (3, 2)]),
-        "glider_gun": ((200, 100), [(51, 76), (52, 74), (52, 76), (53, 64), (53, 65), (53, 72), (53, 73), (53, 86), (53, 87), (54, 63), (54, 67), (54, 72), (54, 73), (54, 86), (54, 87), (55, 52), (55, 53), (55, 62), (55, 68), (55, 72), (55, 73), (56, 52), (56, 53), (56, 62), (56, 66), (56, 68), (56, 69), (56, 74), (56, 76), (57, 62), (57, 68), (57, 76), (58, 63), (58, 67), (59, 64), (59, 65)]),
-        "space_ship": ((25, 25), [(11, 13), (11, 14), (12, 11), (12, 12), (12, 14), (12, 15), (13, 11), (13, 12), (13, 13), (13, 14), (14, 12), (14, 13)]),
-        "die_hard": ((100, 100), [(51, 57), (52, 51), (52, 52), (53, 52), (53, 56), (53, 57), (53, 58)]),
-        "pulsar": ((17, 17), [(2, 4), (2, 5), (2, 6), (7, 4), (7, 5), (7, 6), (9, 4), (9, 5), (9, 6), (14, 4), (14, 5), (14, 6), (2, 10), (2, 11), (2, 12), (7, 10), (7, 11), (7, 12), (9, 10), (9, 11), (9, 12), (14, 10), (14, 11), (14, 12), (4, 2), (5, 2), (6, 2), (4, 7), (5, 7), (6, 7), (4, 9), (5, 9), (6, 9), (4, 14), (5, 14), (6, 14), (10, 2), (11, 2), (12, 2), (10, 7), (11, 7), (12, 7), (10, 9), (11, 9), (12, 9), (10, 14), (11, 14), (12, 14)]),
-        "floraison": ((40, 40), [(19, 18), (19, 19), (19, 20), (20, 17), (20, 19), (20, 21), (21, 18), (21, 19), (21, 20)]),
-        "block_switch_engine": ((400, 400), [(201, 202), (201, 203), (202, 202), (202, 203), (211, 203), (212, 204), (212, 202), (214, 204), (214, 201), (215, 201), (215, 202), (216, 201)]),
-        "u": ((200, 200), [(101, 101), (102, 102), (103, 102), (103, 101), (104, 103), (105, 103), (105, 102), (105, 101), (105, 105), (103, 105), (102, 105), (101, 105), (101, 104)]),
-        "flat": ((200, 400), [(80, 200), (81, 200), (82, 200), (83, 200), (84, 200), (85, 200), (86, 200), (87, 200), (89, 200), (90, 200), (91, 200), (92, 200), (93, 200), (97, 200), (98, 200), (99, 200), (106, 200), (107, 200), (108, 200), (109, 200), (110, 200), (111, 200), (112, 200), (114, 200), (115, 200), (116, 200), (117, 200), (118, 200)])
-    }
-    choice = "boat"
-    if len(sys.argv) > 1:
-        choice = sys.argv[1]
-    resx = 800
-    resy = 800
-    if len(sys.argv) > 3:
-        resx = int(sys.argv[2])
-        resy = int(sys.argv[3])
-    print(f"Pattern initial choisi : {choice}")
-    print(f"resolution ecran : {resx, resy}")
-    try:
-        init_pattern = dico_patterns[choice]
-    except KeyError:
-        print("No such pattern. Available ones are:", dico_patterns.keys())
-        exit(1)
-    grid = Grille(*init_pattern)
-    appli = App((resx, resy), grid)
+    comm = MPI.COMM_WORLD.Dup()
+    nbp = comm.size
+    rank = comm.rank
+
+    if rank == 0:
+        pg.init()
+        resx = 800
+        resy = 800
+        if len(sys.argv) > 3:
+            resx = int(sys.argv[2])
+            resy = int(sys.argv[3])
+        print(f"resolution ecran : {resx, resy}")
+        req = comm.irecv(source=1, tag=11)
+        grid = req.wait()
+        appli = App((resx, resy), grid)
+        send = False
+
+    if rank == 1:
+        send = False
+        dico_patterns = {  # Dimension et pattern dans un tuple
+            "blinker": ((5, 5), [(2, 1), (2, 2), (2, 3)]),
+            "toad": ((6, 6), [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (3, 5)]),
+            "acorn": ((100, 100), [(51, 52), (52, 54), (53, 51), (53, 52), (53, 55), (53, 56), (53, 57)]),
+            "beacon": ((6, 6), [(1, 3), (1, 4), (2, 3), (2, 4), (3, 1), (3, 2), (4, 1), (4, 2)]),
+            "boat": ((5, 5), [(1, 1), (1, 2), (2, 1), (2, 3), (3, 2)]),
+            "glider": ((100, 90), [(1, 1), (2, 2), (2, 3), (3, 1), (3, 2)]),
+            "glider_gun": ((200, 100), [(51, 76), (52, 74), (52, 76), (53, 64), (53, 65), (53, 72), (53, 73), (53, 86), (53, 87), (54, 63), (54, 67), (54, 72), (54, 73), (54, 86), (54, 87), (55, 52), (55, 53), (55, 62), (55, 68), (55, 72), (55, 73), (56, 52), (56, 53), (56, 62), (56, 66), (56, 68), (56, 69), (56, 74), (56, 76), (57, 62), (57, 68), (57, 76), (58, 63), (58, 67), (59, 64), (59, 65)]),
+            "space_ship": ((25, 25), [(11, 13), (11, 14), (12, 11), (12, 12), (12, 14), (12, 15), (13, 11), (13, 12), (13, 13), (13, 14), (14, 12), (14, 13)]),
+            "die_hard": ((100, 100), [(51, 57), (52, 51), (52, 52), (53, 52), (53, 56), (53, 57), (53, 58)]),
+            "pulsar": ((17, 17), [(2, 4), (2, 5), (2, 6), (7, 4), (7, 5), (7, 6), (9, 4), (9, 5), (9, 6), (14, 4), (14, 5), (14, 6), (2, 10), (2, 11), (2, 12), (7, 10), (7, 11), (7, 12), (9, 10), (9, 11), (9, 12), (14, 10), (14, 11), (14, 12), (4, 2), (5, 2), (6, 2), (4, 7), (5, 7), (6, 7), (4, 9), (5, 9), (6, 9), (4, 14), (5, 14), (6, 14), (10, 2), (11, 2), (12, 2), (10, 7), (11, 7), (12, 7), (10, 9), (11, 9), (12, 9), (10, 14), (11, 14), (12, 14)]),
+            "floraison": ((40, 40), [(19, 18), (19, 19), (19, 20), (20, 17), (20, 19), (20, 21), (21, 18), (21, 19), (21, 20)]),
+            "block_switch_engine": ((400, 400), [(201, 202), (201, 203), (202, 202), (202, 203), (211, 203), (212, 204), (212, 202), (214, 204), (214, 201), (215, 201), (215, 202), (216, 201)]),
+            "u": ((200, 200), [(101, 101), (102, 102), (103, 102), (103, 101), (104, 103), (105, 103), (105, 102), (105, 101), (105, 105), (103, 105), (102, 105), (101, 105), (101, 104)]),
+            "flat": ((200, 400), [(80, 200), (81, 200), (82, 200), (83, 200), (84, 200), (85, 200), (86, 200), (87, 200), (89, 200), (90, 200), (91, 200), (92, 200), (93, 200), (97, 200), (98, 200), (99, 200), (106, 200), (107, 200), (108, 200), (109, 200), (110, 200), (111, 200), (112, 200), (114, 200), (115, 200), (116, 200), (117, 200), (118, 200)])
+        }
+        choice = "glider"
+        if len(sys.argv) > 1:
+            choice = sys.argv[1]
+        print(f"Pattern initial choisi : {choice}")
+        try:
+            init_pattern = dico_patterns[choice]
+        except KeyError:
+            print("No such pattern. Available ones are:", dico_patterns.keys())
+            exit(1)
+        grid = Grille(*init_pattern)
+        req = comm.isend(grid, dest=0, tag=11)
+        req.wait()
 
     loop = True
     while loop:
-        # time.sleep(0.1) # A régler ou commenter pour vitesse maxi●●
-        t1 = time.time()
-        diff = grid.compute_next_iteration()
-        t2 = time.time()
-        appli.draw()
-        t3 = time.time()
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                loop = False
-        print(f"Temps calcul prochaine generation : {t2 - t1: 2.2e} secondes, temps affichage : {t3 - t2: 2.2e} secondes\r", end='')
-
-pg.quit()
+        time.sleep(1)  # A régler ou commenter pour vitesse maxi●●
+        if rank == 1:
+            t1 = time.time()
+            diff = grid.compute_next_iteration()
+            t2 = time.time()
+            print(f"Temps calcul prochaine generation : {t2 - t1: 2.2e} secondes\n", end='')
+            req = comm.irecv(source=0, tag=12)
+            req.Wait()
+            if send:
+                comm.send(grid, dest=0, tag=11)
+                send = False
+        if rank == 0:
+            req = comm.isend(True, dest=1, tag=12)
+            grid = comm.recv(source=1, tag=11)
+            t1 = time.time()
+            appli.draw(grid)
+            t2 = time.time()
+            print(f"Temps affichage : {t2 - t1: 2.2e} secondes\n", end='')
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    loop = False
+    if rank == 0:
+        pg.quit()
